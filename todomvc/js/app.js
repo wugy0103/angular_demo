a06bca06fdd128e3412c10340a79df8627d0fff3
wuguoyuan <13611463095@163.com>
2016-09-07 11:07:23 +0800

    删除数据ok

--- todomvc/js/app.js
+++ todomvc/js/app.js
@@ -8,15 +8,27 @@
 				id:0,name:"吃饭",completed:true
 			},
 			{
-				id:0,name:"睡觉",completed:false
+				id:1,name:"睡觉",completed:false
 			},
 			{
-				id:0,name:"打豆豆",completed:false
+				id:2,name:"打豆豆",completed:false
 			},
 			{
-				id:0,name:"学习",completed:true
+				id:3,name:"学习",completed:true
 			}
-		]
+		];
+
+		// 删除数据
+		$scope.remove = function(id){
+			angular.forEach($scope.tasks,function(data,index){
+				if(data.id == id){
+					$scope.tasks.splice(index,1);
+				}
+				return; 
+			})
+		}
+
+
 	}])
 
 })(window,angular);
