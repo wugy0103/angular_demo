ea8b8df39054af6622f6df8a80b7c89dba3eeca9
wuguoyuan <13611463095@163.com>
2016-09-07 10:32:07 +0800

    数据展示ok

--- todomvc/js/app.js
+++ todomvc/js/app.js
@@ -1,6 +1,22 @@
-(function (window) {
+(function (window,angular) {
 	'use strict';
+	var app = angular.module("myApp",[]);
+	app.controller("todoController",["$scope",function($scope){
+		// 数据展示
+		$scope.tasks = [
+			{
+				id:0,name:"吃饭",completed:true
+			},
+			{
+				id:0,name:"睡觉",completed:false
+			},
+			{
+				id:0,name:"打豆豆",completed:false
+			},
+			{
+				id:0,name:"学习",completed:true
+			}
+		]
+	}])
 
-	// Your starting point. Enjoy the ride!
-
-})(window);
+})(window,angular);
